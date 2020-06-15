f68460c818d74415893c72965534b73126420998
Leo Zhao <Leo.Zhao@D2L.com>
2020-06-15 10:41:16 -0400

    Initial Commit

---
+++ SwiftUITryoutTests/SwiftUITryoutTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUITryoutTests.swift
+//  SwiftUITryoutTests
+//
+//  Created by Leo Zhao on 2020-06-15.
+//  Copyright © 2020 Leo Zhao. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUITryout
+
+class SwiftUITryoutTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
