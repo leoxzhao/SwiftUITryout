94c9064673028a053f6b65d724259cb21ccddb58
Leo Zhao <Leo.Zhao@D2L.com>
2020-06-18 10:32:28 -0400

    Basic Gesture

--- SwiftUITryout/GesturesView.swift
+++ SwiftUITryout/GesturesView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct GesturesView: View {
+    @State var tapMessage: String = "Try to tap on me."
+    @State var longPressMessage: String = "Try to long press on me."
+    @State private var currentAmount: Angle = .degrees(0)
+    @State private var finalAmount: Angle = .degrees(0)
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(tapMessage)
+                .onTapGesture(count: 1) { self.tapMessage = "Tap" }
+            Text(longPressMessage)
+                .onLongPressGesture { self.longPressMessage = "Long Pressed" }
+            Text("Rotate Me!")
+                .frame(width: 100, height: 100, alignment: .center)
+                .rotationEffect(currentAmount + finalAmount)
+                .gesture(
+                    RotationGesture()
+                        .onChanged { angle in
+                            self.currentAmount = angle
+                        }
+                        .onEnded { angle in
+                            self.finalAmount += self.currentAmount
+                            self.currentAmount = .degrees(0)
+                        }
+                )
+        }
     }
 }
 
