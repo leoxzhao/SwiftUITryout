207c221828174f6a2bd5eaffbd2ae85a2c0d2b5f
Leo Zhao <Leo.Zhao@D2L.com>
2020-06-22 10:00:56 -0400

    All kinds of different borders.

---
+++ SwiftUITryout/BordersDemoView.swift
@@ -0,0 +1,73 @@
+//
+//  BordersDemoView.swift
+//  SwiftUITryout
+//
+//  Created by Leo Zhao on 2020-06-19.
+//  Copyright © 2020 Leo Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(Color.black)
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .cornerRadius(10)
+    }
+}
+
+struct BordersDemoView: View {
+    var body: some View {
+        VStack {
+            Text("Text Rectangle Border")
+                .font(.largeTitle)
+                .padding()
+                .border(Color.black)
+            Spacer()
+                .frame(height: 20)
+            Text("Border + Radius")
+                .font(.largeTitle)
+                .padding()
+                .border(Color.red, width: 3)
+                .cornerRadius(15)
+            Spacer()
+                .frame(height: 20)
+            Text("Overlay")
+                .font(.largeTitle)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(lineWidth: 2)
+                )
+            Spacer()
+                .frame(height: 20)
+            Text("Dashed Border")
+                .font(.largeTitle)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(style: StrokeStyle(lineWidth: 4, dash: [15.0]))
+                )
+            Spacer()
+                .frame(height: 20)
+            Image(systemName: "power")
+                .font(.system(size: 56.0))
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .stroke(lineWidth: 2.0)
+                )
+            Text("Modifier")
+                .modifier(MyViewModifier())
+        }
+    }
+}
+
+struct BordersDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        BordersDemoView()
+    }
+}
